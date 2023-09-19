6b6290e2ef68ec850c7b19ea4a5b87367df2a415
Monet <monetpaul23@gmail.com>
2023-09-19 12:54:34 -0400

    working on  quality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Nav from './Comps/Nav'
 import Started from './Comps/Started';
 import Features from './Comps/Features';
+import Quality from './Comps/Quality';
 function App() {
   return (
     <div className="App">
       <Nav />
       <Started />
       <Features />
+      <Quality />
     </div>
   );
 }
---
+++ src/Comps/Quality.js
@@ -0,0 +1,18 @@
+export default function Quality (){
+   
+   
+    return (
+        <div className="container--big-row">
+        <div className="quality-container">
+        <div className="quality-description">
+            <h3>QUALITY HOSTING</h3>
+            <h1>Where quality hosting meets exceptional performance.</h1>
+            <p>Our cutting-edge infrastructure and unwavering commitment to excellence ensure your website enjoys blazing-fast speeds, unbeatable uptime, and top-notch security. Discover a hosting experience that's tailor-made for your success – from shared hosting to dedicated servers, we're here to elevate your online presence to new heights.</p>
+        </div>
+<figure className="quality--img-container"><img  src="undraw_pet_adoption_-2-qkw.svg"/></figure>
+
+        </div>
+        </div>
+        
+        )
+}
