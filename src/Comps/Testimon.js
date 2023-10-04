8ed09a566d373888d256e348c53dd34fa1d0869a
Monet <monetpaul23@gmail.com>
2023-10-04 08:18:01 -0400

    working on photos  for testimon Comp

--- src/Comps/Testimon.js
+++ src/Comps/Testimon.js
@@ -24,24 +24,27 @@ export default function  Testimon (props){
 
     return (
         <div className="container--big-row">
-        <div className="steps-container">
+        <div className="test-container">
         <figure className="content--img-container"><img  src="cats-paw-5325371.png"/></figure>
         <div className="content--description">
             <h3 className="content-section--2title special-color content--text-right">TESTIMONIALS</h3>
             <h1 className="content-section--1title content--text-right">Our <span className="special-color-1">Clowder</span> Love Us.</h1>
-            <p className="content-section--para content-section--para-left">Each plan is designed to cater to different levels of hosting needs and budgets, ensuring that customers can choose the plan that best suits their specific requirements. As their websites and online ventures grow, they can easily upgrade to higher-tier plans for more resources, features, and support.</p>
+            <p className="content-section--para content-section--para-left">Hear from some  of  our customers  and  what they think about our company</p>
+            <br/>
             <div>
-                <div>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>  
+                <div className='test--stars'>
+                <i class="fa-solid fa-star test--star-space"></i>
+                <i class="fa-solid fa-star test--star-space"></i>
+                <i class="fa-solid fa-star test--star-space"></i>
+                <i class="fa-solid fa-star test--star-space"></i>
+                <i class="fa-solid fa-star test--star-space"></i>  
                 </div>
-                <h1>{content.title}</h1>
+                <h3 className='content--text-right'>{content.title}</h3>
+                <br/>
                 <p>{content.para}</p>
+                <br/>
                 <div>
-                 <figure><img  src={`testimonals/${content.picImg}`} /></figure>
+                 <figure className='test--photo-container'><img   className='test--photo' src={`testimonals/${content.picImg}`} /></figure>
                  <h3>{content.name}</h3>  
                 </div>
                 <button onClick={()=> setCount(prevCount =>prevCount-1)}><i class="fa-solid fa-arrow-left"></i></button>
