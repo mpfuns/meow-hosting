78f1af937e0b06a3148c3a8ffac589fb2316aee9
Monet <monetpaul23@gmail.com>
2023-10-07 14:21:00 -0400

    working on FAQ

--- src/Comps/FAQ.js
+++ src/Comps/FAQ.js
@@ -2,20 +2,26 @@ import React from "react"
 
 export default function FAQ(){
    const [questions, setQuestions] = React.useState([{id: 1, open: false},{id: 2, open: false},{id: 3, open: false}, {id: 4, open: false}, {id: 5, open: false}])
+    
+
+
 
    function questionOpen (num){
-        setQuestions(oldQuestions => oldQuestions.map(part =>{
+    
+
+    setQuestions(oldQuestions => oldQuestions.map(part =>{
             if(part.id === num){
                 return {...part, open: !part.open }
             } else{
                 return part
+                //{...part, open: !part.open }
             }
         }))
-
+      
 
    }
-   console.log(questions[0])
 
+   
    
     return (
         <div className="container--big-row ">
@@ -27,12 +33,58 @@ export default function FAQ(){
                     <div className="faq-item">
                         <div className="faq-item--group">
                             <h2>How does security work with your hosting services? </h2>
-                            <button className="faq-item--button" onClick={() => questionOpen(1)}><i class="fa-solid fa-angle-down"></i></button>
+                            <button className="faq-item--button"  onClick={() => questionOpen(1)}style={ 
+                                questions[0].id=== 1& questions[0].open ? { transform: 'rotate(-180deg) translatez(0px)',
+                                transition: "all 0.3s ease 0s"} :
+                                { transform: 'none',
+                                transition: "all 0.3s ease 0s"} } ><i class="fa-solid fa-angle-down"></i></button>
                         </div>
-                        { questions[0].open? <p>tttt</p> :<p></p> }
+                        { questions[0].open? <p>Our hosting services include various security measures such as firewalls, malware scanning, DDoS protection, and SSL certificates to safeguard your website and data. Regular backups are also provided to ensure data recovery in case of any unforeseen events.</p> :<p></p> }
                     </div>
-
-
+                    <div className="faq-item">
+                        <div className="faq-item--group">
+                            <h2>Can I upgrade my hosting plan as my website grows?</h2>
+                            <button className="faq-item--button"  onClick={() => questionOpen(2)}style={ 
+                                questions[1].id=== 2& questions[1].open ? { transform: 'rotate(-180deg) translatez(0px)',
+                                transition: "all 0.3s ease 0s"} :
+                                { transform: 'none',
+                                transition: "all 0.3s ease 0s"} } ><i class="fa-solid fa-angle-down"></i></button>
+                        </div>
+                        { questions[1].open? <p>Absolutely! We understand that your hosting needs may evolve over time. You can easily upgrade your hosting plan to accommodate increased traffic, resources, and features. Our seamless upgrade process ensures minimal downtime during the transition.</p> :<p></p> }
+                    </div>
+                    <div className="faq-item">
+                        <div className="faq-item--group">
+                            <h2>Do you offer website migration services? </h2>
+                            <button className="faq-item--button"  onClick={() => questionOpen(3)}style={ 
+                                questions[2].id=== 3& questions[2].open ? { transform: 'rotate(-180deg) translatez(0px)',
+                                transition: "all 0.3s ease 0s"} :
+                                { transform: 'none',
+                                transition: "all 0.3s ease 0s"} } ><i class="fa-solid fa-angle-down"></i></button>
+                        </div>
+                        { questions[2].open? <p>Yes, we provide website migration assistance for customers transferring from other hosting providers. Our support team will guide you through the process, ensuring a smooth migration of your website and data.</p> :<p></p> }
+                    </div>
+                    <div className="faq-item">
+                        <div className="faq-item--group">
+                            <h2>Can I register a domain through your hosting service? </h2>
+                            <button className="faq-item--button"  onClick={() => questionOpen(4)}style={ 
+                                questions[3].id=== 4& questions[3].open ? { transform: 'rotate(-180deg) translatez(0px)',
+                                transition: "all 0.3s ease 0s"} :
+                                { transform: 'none',
+                                transition: "all 0.3s ease 0s"} } ><i class="fa-solid fa-angle-down"></i></button>
+                        </div>
+                        { questions[3].open? <p>Yes, we offer domain registration services, and many of our hosting plans include a free domain for the first year. You can also transfer your existing domain to us for hassle-free management.</p> :<p></p> }
+                    </div>
+                    <div className="faq-item">
+                        <div className="faq-item--group">
+                            <h2>What control panel do you provide for managing my hosting?</h2>
+                            <button className="faq-item--button"  onClick={() => questionOpen(5)}style={ 
+                                questions[4].id=== 5& questions[4].open ? { transform: 'rotate(-180deg) translatez(0px)',
+                                transition: "all 0.3s ease 0s"} :
+                                { transform: 'none',
+                                transition: "all 0.3s ease 0s"} } ><i class="fa-solid fa-angle-down"></i></button>
+                        </div>
+                        { questions[4].open? <p>Yes, we offer domain registration services, and many of our hosting plans include a free domain for the first year. You can also transfer your existing domain to us for hassle-free management.</p> :<p></p> }
+                    </div>
                 </div>
 
             </div>
