b72010fee298fa1eab42cf0ee9af7ddd56ef9768
Monet <monetpaul23@gmail.com>
2023-10-15 13:25:06 -0400

    working on interction observer

--- src/App.js
+++ src/App.js
@@ -11,11 +11,36 @@ import data from './data';
 import FAQ from './Comps/FAQ';
 import Intro from './Comps/Intro';
 import Footer from './Comps/Footer';
+import React, { useEffect, useState, useRef } from 'react';
 function App() {
+
+  const startRef= useRef();
+  const [sections, setSections]= useState([]);
+console.log('sections', sections)
+
+useEffect(() =>{ 
+   const observer = new IntersectionObserver((entries) =>{
+    const entry = entries[0];
+      setSections(entry.isIntersecting)
+   
+
+   })
+   observer.observe(startRef.current)
+},[])
+
+
+
+
   return (
     <div className="App">
+     
       <Nav />
+     
+    
+      <section  ref={startRef}>
       <Started />
+      </section>
+      
       <Features />
       <Quality />
       <Steps />
