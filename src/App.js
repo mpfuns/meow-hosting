7972f463cd337ddb62646ac36459397fe45a6e65
Monet <monetpaul23@gmail.com>
2023-10-15 16:56:28 -0400

    working on animation

--- src/App.js
+++ src/App.js
@@ -13,20 +13,52 @@ import Intro from './Comps/Intro';
 import Footer from './Comps/Footer';
 import React, { useEffect, useState, useRef } from 'react';
 function App() {
-
+ 
   const startRef= useRef();
-  const [sections, setSections]= useState([]);
-console.log('sections', sections)
+  const featRef= useRef();
+  const QualRef= useRef();
+  const [sections, setSections]= useState([{answer:false}, {answer:false}, {answer:false}]);
+
+console.log(sections)
+
+
+
 
 useEffect(() =>{ 
-   const observer = new IntersectionObserver((entries) =>{
+ /*
+  const observer = new IntersectionObserver((entries) =>{
     const entry = entries[0];
-      setSections(entry.isIntersecting)
+    console.log(entries)
+    entries.map( entry=>{
+      const placeInList= entries.indexOf(entry)
+     setSections(Prev =>{ Prev.map(part=>{
+      const piece= part.findIndex((num)=> num === placeInList)
+      if(piece.answer !== entry.isIntersecting){
+        return{
+          answer:entry.isIntersecting
+        }
+      }
+     }
+     
+     )
+     
+     }
+      )
+    })
+
+   // setSections(entry.isIntersecting)
+   //map  the  change to  make it  object 
    
 
    })
-   observer.observe(startRef.current)
-},[])
+   // control what observer
+    observer.observe(startRef.current)
+    observer.observe(featRef.current)
+    observer.observe(QualRef.current)
+
+   */
+
+},[sections])
 
 
 
@@ -37,12 +69,17 @@ useEffect(() =>{
       <Nav />
      
     
-      <section  ref={startRef}>
+      <section  className='' ref={startRef}>
       <Started />
       </section>
       
-      <Features />
+      <section className=''ref={featRef}>
+        <Features />
+      </section>
+      <section className='hidden-section--left' ref={QualRef} >
       <Quality />
+      </section>
+     
       <Steps />
       <Value />
       <Pricing />
